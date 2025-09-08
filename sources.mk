13235aad89f423c949013675c0e59eb39c47ea57
Rotem Barnea <rotembarnea@pop-os.lan>
2025-09-08 10:15:06 +0300

    simplify makefile

---
+++ sources.mk
@@ -0,0 +1,13 @@
+# NSphere Source Files
+
+SOURCES = nsphere.c \
+          logging.c \
+          sidm.c \
+          globals.c \
+          particle_array_ops.c \
+          particle_data.c \
+          exit.c \
+          utils.c \
+          signal_processing.c \
+          dynamics.c \
+          cli.c
